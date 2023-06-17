0ec26764f0fa8b7de447175bda5928b009b35463
Sean <soxfansean@gmail.com>
2023-06-17 18:43:22 -0400

    Create operate function

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function divide(first, second){
     }
     return parseFloat((first / second).toFixed(2));
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1, num2, operator){
+    if(operator === "+"){
+        return add(num1, num2);
+    }else if(operator === "-"){
+        return subtract(num1, num2);
+    }else if(operator === "*"){
+        return multiply(num1, num2);
+    }else if(operator === "/"){
+        return divide(num1, num2);
+    }
+}
